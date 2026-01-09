4f327ceedeb4f9f28ea09fa1ac761762c7e1222d
daneena28 <daneena28@gmail.com>
2026-01-09 20:34:11 +0000

    change game to be resizable instead of static fullscreen

--- Game/lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/Lwjgl3Launcher.java
+++ Game/lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/Lwjgl3Launcher.java
@@ -38,7 +38,10 @@ public class Lwjgl3Launcher {
         //// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
         //// useful for testing performance, but can also be very stressful to some hardware.
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
-        configuration.setFullscreenMode(configuration.getDisplayMode());
+        //// CHANGED: Use windowed mode instead of fullscreen for testing
+        configuration.setWindowedMode(1280, 960);  // TESTING RESIZABLE WINDOW
+        configuration.setResizable(true);           // TESTING
+        //configuration.setFullscreenMode(configuration.getDisplayMode());
         //// You can change these files; they are in lwjgl3/src/main/resources/ .
         //// They can also be loaded from the root of assets/ .
         configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
